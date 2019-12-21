03e12930bf3b5b5d2b57b3ce7bcb6f5df3c2cddc
prostomarkeloff <somefuckingidiotfrominternet@pm.me>
2019-12-21 23:04:32 +0300

    fix plain stylish issue

--- vbml/patcher/recursion.py
+++ vbml/patcher/recursion.py
@@ -2,7 +2,7 @@ class RecursionArgument:
     def __init__(self, value: str, **context):
         if (value[0], value[-1]) != ('"', '"'):
             raise ValueError(
-                'vbml Recursion argument\'s inclusion should be maintained by the "..." separators'
+                'VBML\'s recursion argument\'s inclusion should be maintained by the "..." separators'
             )
         self._value = value[1:-1]
         self._pattern = {"text": self.value, **context}
